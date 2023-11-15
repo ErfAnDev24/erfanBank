bada95f07d8cd7737cbf0b846db7ed245989e20d
erfan <erfan01724@gmail.com>
2023-11-15 13:27:42 +0330

    gatewayserver

--- erfanBank/getwayserver/src/main/java/com/example/getwayserver/filters/ResponseTraceFilter.java
+++ erfanBank/getwayserver/src/main/java/com/example/getwayserver/filters/ResponseTraceFilter.java
@@ -1,17 +1,33 @@
 package com.example.getwayserver.filters;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
 
 @Component
 public class ResponseTraceFilter {
 
-  /*  @Autowired
+    @Autowired
     private FilterUtility filterUtility;
 
+    private final static Logger logger = LoggerFactory.getLogger(ResponseTraceFilter.class);
 
+    @Bean
+    public GlobalFilter postGlobalFilter(){
+        return (exchange, chain) -> {
+                return chain.filter(exchange).then(Mono.fromRunnable(() -> {
+                HttpHeaders httpHeaders = exchange.getRequest().getHeaders();
+                String correlationId = filterUtility.getCorrelationId(httpHeaders);
+                exchange.getRequest().getHeaders().add(filterUtility.CORRELATION_ID,correlationId);
+                logger.debug("erfanbank_correlation_id has been UPDATED in ResponseTraceFilter");
+            }));
+        };
     }
-*/
 }
